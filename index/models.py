593ca1ee9bad753f359cd85ba6e438263ead4228
sergey <sergio@dezigner.ru>
2020-10-09 00:12:11 +0300

    Добавлена отзывчивость base.html. Мелкие исправления и улучшения дизайна главной страницы

--- index/models.py
+++ index/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class Group(models.Model):
-    name = models.CharField(verbose_name='Название группы',max_length=30)
+    name = models.CharField(verbose_name='Название группы',max_length=50)
     class Meta:
         verbose_name = 'Группа слайдов'
         verbose_name_plural = 'Группы слайдов'
+        ordering = ['name']
     def __str__(self):
         return self.name
 
@@ -13,14 +14,14 @@ class Slide(models.Model):
     """Слайды на главной"""
     slide_group = models.ForeignKey(Group, on_delete=models.DO_NOTHING)
     slide_img = models.ImageField(verbose_name='Изображение слайда',upload_to='index/slide/')
-    slide_description = models.TextField(verbose_name='Текст / Описание',blank=True, null=True)
+    slide_description = models.TextField(verbose_name='Текст / Описание (h2 / p)',blank=True, null=True)
     slide_link_name = models.CharField(verbose_name='Название кнопки',blank=True, null=True, max_length=100, default='Перейти')
     slide_link_url = models.SlugField(verbose_name='Ссылка',blank=True, null=True)
     slide_order = models.IntegerField(verbose_name='Порядок', default='0')
     slide_visible = models.BooleanField(verbose_name='Показывать?', default=True)
 
     class Meta:
-        ordering = ['slide_order', 'id']
+        ordering = ['slide_group', 'slide_order', 'id']
         verbose_name = 'Слайд'
         verbose_name_plural = 'Слайды'
 
--- index/views.py
+++ index/views.py
@@ -3,8 +3,11 @@ from .models import Slide
 
 def index(request):
     main_slides = Slide.objects.filter(slide_group=1, slide_visible=True)
-
+    slides_2 = Slide.objects.filter(slide_group=2, slide_visible=True)
+    banner = Slide.objects.filter(slide_group=3, slide_visible=True)[0]
     context = {
         'main_slides': main_slides,
+        'slides_2': slides_2,
+        'banner': banner
                }
     return render(request, 'index/index.html', context)
